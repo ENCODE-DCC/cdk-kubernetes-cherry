ac399f1835aef9925913598aeca669d87cd0e7ed
Otto Jolanki <ojolanki@stanford.edu>
2025-06-02 08:22:58 -0700

    test kubernetesjoboperator

---
+++ dags/test_kubernetes_job.py
@@ -0,0 +1,31 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.job import KubernetesJobOperator
+
+
+default_args = {
+    'owner': 'me',
+    'depends_on_past': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+
+with DAG(
+    dag_id='test-kubernetes-job',
+    default_args=default_args,
+    description='A DAG to run a Kubernetes job',
+    schedule_interval=None,
+    max_active_runs=1,
+    catchup=False,
+) as dag:
+
+    kubernetes_job = KubernetesJobOperator(
+        task_id='kubernetes-test-job',
+        namespace='data-stack-dev',
+        image='debian:latest',
+        command=['bash', '-c', 'echo "Hello, World!"'],
+        name='test-kubernetes-job',
+        wait_until_job_complete=True,
+    )
